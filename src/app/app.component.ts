4aad95324033083f8c6777127bf86b450f32eb0c
connorbarkr <connorbarkr@gmail.com>
2018-02-13 15:41:44 -0500

    Cosmetic changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,82 @@
-import { Component } from '@angular/core';
-import { fadeInAnimation } from './_animations/fade-in.animation';
+import { Component, OnInit, AfterViewInit, trigger, state, animate, transition, style } from '@angular/core';
 import * as sha1 from 'sha1';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass'],
-  animations: [fadeInAnimation],
-  host: { '[@fadeInAnimation]': '' }
+  animations: [
+    trigger('visible', [
+        state('shown', style({
+            opacity: 1
+        })),
+        state('hidden', style({
+            opacity: 0
+        })),
+        transition('hidden => shown', animate('1s')),
+        transition('shown => hidden', animate('0.2s'))
+    ])
+  ],
 })
 export class AppComponent {
 
-  authedUser = '';
-  password_rea = '794bfa57bb763388d961a98aa5b9f44e81e63a26';
-  password_connor = '31672fd92b13150c3b261144d67626552e4f75c9';
+  titleIsVisible: string = 'hidden';
+  passwordIsVisible: string = 'hidden';
+  footerIsVisible: string = 'hidden';
+  noteIsVisible: string = 'hidden';
+  authed: boolean = false;
+  authedUser: string = '';
+  password_rea: string = '794bfa57bb763388d961a98aa5b9f44e81e63a26';
+  password_connor: string = '31672fd92b13150c3b261144d67626552e4f75c9';
   password: string;
-  authed = false;
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.toggleVisibility('title');
+    }, 300);
+    setTimeout(() => {
+      this.toggleVisibility('password');
+    }, 400);
+    setTimeout(() => {
+      this.toggleVisibility('footer');
+    }, 500);
+  }
 
   authenticate(password_plaintext) {
     let password_crypto = sha1(password_plaintext)
     if (password_crypto == this.password_rea) {
-      this.authedUser = 'rea'
-      this.authed = true
+      this.passwordIsVisible = this.passwordIsVisible == 'shown' ? 'hidden' : 'shown';
+      this.authedUser = 'rea';
+      setTimeout(() => {
+        this.authed = true;
+      }, 200);
+      this.toggleVisibility('note');
     }
     else if (password_crypto == this.password_connor) {
+      this.passwordIsVisible = this.passwordIsVisible == 'shown' ? 'hidden' : 'shown';
       this.authedUser = 'connor'
-      this.authed = true
+      setTimeout(() => {
+        this.authed = true;
+      }, 200);
     } else {
       this.authed = false
+    }
+  }
+
+  toggleVisibility(item: string) {
+    switch (item) {
+      case 'title':
+        this.titleIsVisible = this.titleIsVisible == 'shown' ? 'hidden' : 'shown';
+        break;
+      case 'password':
+        this.passwordIsVisible = this.passwordIsVisible == 'shown' ? 'hidden' : 'shown';
+        break;
+      case 'note':
+        this.noteIsVisible = this.noteIsVisible == 'shown' ? 'hidden' : 'shown';
+        break;
+      case 'footer':
+        this.footerIsVisible = this.footerIsVisible == 'shown' ? 'hidden' : 'shown';
+        break;
     }
   }
 }
--- src/app/notes/notes.component.ts
+++ src/app/notes/notes.component.ts
@@ -1,37 +1,55 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, OnInit, AfterViewInit, trigger, state, animate, transition, style } from '@angular/core';
 
 import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+
 import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-notes',
   templateUrl: './notes.component.html',
-  styleUrls: ['./notes.component.sass']
+  styleUrls: ['./notes.component.sass'],
+  animations: [
+    trigger('visibility', [
+        state('shown', style({
+            opacity: 1
+        })),
+        state('hidden', style({
+            opacity: 0
+        })),
+        transition('* => *', animate('0.2s'))
+    ])
+  ],
 })
-export class NotesComponent implements OnInit {
+export class NotesComponent {
 
   @Input() user: string;
 
+  readIsVisible: string = 'hidden';
+  writeIsVisible: string = 'hidden';
   notesRef: AngularFireList<any>;
   notes: Observable<any[]>;
-  reading = false;
+  reading: boolean = true;
 
   constructor(private db: AngularFireDatabase) {}
 
   ngOnInit() {
-    if (this.user == 'connor') {
+    if (this.user == 'connor')
       this.notesRef = this.db.list('/notes-connor')
-    } else if (this.user == 'rea') {
+    else if (this.user == 'rea')
       this.notesRef = this.db.list('/notes-rea')
-    } else {
-      this.notesRef = this.db.list('/notes-connor')
-    }
+
     this.notes = this.notesRef.snapshotChanges(['child_added', 'child_removed']).map(changes => {
       return changes.map(c => ({
         key: c.payload.key,
         ...c.payload.val()
       }))
     })
+  }
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.toggleVisibility('read');
+    }, 300);
   }
 
   updateDescription(note, description) {
@@ -41,14 +59,36 @@ export class NotesComponent implements OnInit {
   toggleView() {
     switch (this.user) {
       case 'rea':
-        this.user = 'connor'
-        break
+        this.user = 'connor';
+        break;
       case 'connor':
-        this.user = 'rea'
-        break
+        this.user = 'rea';
+        break;
     }
-    this.ngOnInit()
+    this.ngOnInit();
+    if (this.reading) {
+      this.toggleVisibility('read');
+      setTimeout(() => {
+        this.toggleVisibility('write');
+      }, 200);
+    } else {
+      this.toggleVisibility('write');
+      setTimeout(() => {
+        this.toggleVisibility('read');
+      }, 200);
+    }
     this.reading = !this.reading
+  }
+
+  toggleVisibility(item: string) {
+    switch (item) {
+      case 'read':
+        this.readIsVisible = this.readIsVisible == 'shown' ? 'hidden' : 'shown';
+        break;
+      case 'write':
+        this.writeIsVisible = this.writeIsVisible == 'shown' ? 'hidden' : 'shown';
+        break;
+    }
   }
 
 }
